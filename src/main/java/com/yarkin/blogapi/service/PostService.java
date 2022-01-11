73a693f1645a01e7ef04c245aeac4e489d3e2aae
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-01-11 14:42:26 +0200

    Add some methods to post service interface

--- src/main/java/com/yarkin/blogapi/service/PostService.java
+++ src/main/java/com/yarkin/blogapi/service/PostService.java
@@ -5,4 +5,13 @@ import com.yarkin.blogapi.entity.Post;
 import java.util.List;
 
 public interface PostService {
+    List<Post> getAll();
+
+    // Returns added post
+    Post add(Post newPost);
+
+    // Updates by obtained id and received post
+    void update(long postId, Post post);
+
+    void delete(long postId);
 }
