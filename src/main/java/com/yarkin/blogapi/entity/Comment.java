7b4cf71c95c6792bdd1fefb5b6766be9bd928d60
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-01-11 14:33:16 +0200

    Create comment & post entities

--- src/main/java/com/yarkin/blogapi/entity/Comment.java
+++ src/main/java/com/yarkin/blogapi/entity/Comment.java
@@ -1,4 +1,15 @@
 package com.yarkin.blogapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class Comment {
+    private long id;
+    private String text;
+    private String creationDate;
+    private long postId;
 }
--- src/main/java/com/yarkin/blogapi/entity/Post.java
+++ src/main/java/com/yarkin/blogapi/entity/Post.java
@@ -1,4 +1,17 @@
 package com.yarkin.blogapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class Post {
+    private long id;
+    private String title;
+    private String content;
+    private LocalDateTime creationDate;
 }
