f9cebf656c4833a08715fa1a3b5d52d77314ad62
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-01-11 19:30:08 +0200

    Add star field to show top post or not

--- src/main/java/com/yarkin/blogapi/entity/Post.java
+++ src/main/java/com/yarkin/blogapi/entity/Post.java
@@ -3,15 +3,18 @@ package com.yarkin.blogapi.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.hibernate.annotations.Table;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@Table(appliesTo = "posts")
 public class Post {
     private long id;
     private String title;
     private String content;
     private LocalDateTime creationDate;
+    private boolean star;
 }
