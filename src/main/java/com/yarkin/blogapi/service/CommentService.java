62f66cf3ec9b2d1bdb0cd026ce2ac86fd7b05f43
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-01-12 14:12:44 +0200

    Define at comment service add, get all & get by id methods

--- src/main/java/com/yarkin/blogapi/service/CommentService.java
+++ src/main/java/com/yarkin/blogapi/service/CommentService.java
@@ -1,4 +1,17 @@
 package com.yarkin.blogapi.service;
 
+import com.yarkin.blogapi.entity.Comment;
+import com.yarkin.blogapi.entity.Post;
+
+import java.util.List;
+
 public interface CommentService {
+
+    List<Comment> getAllByPostId(long postId);
+
+    Comment getByPostId(long postId, long commentId);
+
+    // Returns added comment
+    Comment add(long postId, Comment newComment);
+
 }
